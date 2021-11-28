8047c0b9fb31c357182565f2444c9a2fbac4f233
Jane Njeri <daktari2033@gmail.com>
2021-11-28 13:27:35 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import { allTasks } from "./tasks";
+
+import AppStyles from "./App.module.css";
 
 function App() {
+  const [tasks, setTasks] = useState(allTasks);
+  const complete = tasks.filter((task) => task.status === "complete");
+  const pending = tasks.filter((task) => task.status === "pending");
+
+  function handleDragStart(e, id) {
+    e.dataTransfer.setData("text/plain", id);
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  function handleDrop(e, status) {
+    const id = e.dataTransfer.getData("text/plain");
+    const newTasks = tasks.filter((task) => {
+      if (task.id === id) {
+        task.status = status;
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={AppStyles.container}>
+      <div className={AppStyles.completetasks}>
+        <p>Complete</p>
+        <div
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={(e) => handleDrop(e, "complete")}
+          className={AppStyles.tasklist}
         >
-          Learn React
-        </a>
-      </header>
+          {complete.map((task) => {
+            return (
+              <div draggable onDragStart={(e) => handleDragStart(e, task.id)}>
+                <p>{task.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div
+        onDragOver={(e) => handleDragOver(e)}
+        onDrop={(e) => handleDrop(e, "pending")}
+        className={AppStyles.pendingtasks}
+      >
+        <p>Pending</p>
+        <div className={AppStyles.tasklist}>
+          {pending.map((task) => {
+            return (
+              <div draggable onDragStart={(e) => handleDragStart(e, task.id)}>
+                <p>{task.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/tasks.js
@@ -0,0 +1,32 @@
+export const allTasks = [
+  {
+    id: "task 1",
+    name: "react",
+    status: "complete",
+  },
+  {
+    id: "task 2",
+    name: "redux",
+    status: "complete",
+  },
+  {
+    id: "task 3",
+    name: "react-router",
+    status: "complete",
+  },
+  {
+    id: "task 4",
+    name: "redux-saga",
+    status: "pending",
+  },
+  {
+    id: "task 5",
+    name: "redux-thunk",
+    status: "pending",
+  },
+  {
+    id: "task 6",
+    name: "redux-form",
+    status: "pending",
+  },
+];
